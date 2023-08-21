465ac27965fa1536d8ef2616c51fd0cfb9dbe9c2
14239 <14239@naver.com>
2023-08-21 23:09:46 +0900

    CSV 클래스 명칭 정리

---
+++ Models/Data.cs
@@ -0,0 +1,63 @@
+﻿namespace SMPage.Models
+{
+    // 언어, 국가 세팅
+
+    public class VersionCSV // 개별 버전 (versions.csv)
+    {
+        public int Id { get; set; }
+        public int VersionGroupId { get; set; }
+        public string Identifier { get; set; }
+    }
+    public class VersionGroupCSV // 같은 버전끼리 묶어서 셈(ex 레드,그린) (version_groups.csv)
+    {
+        public int Id { get; set; }
+        public string Identifier { get; set; }
+        public int GenerationId { get; set; }
+        public int Order { get; set; }
+    }
+
+    public class VersionNameCSV // (version_names.csv)
+    {
+        public int VersionId { get; set; }
+        public int LocalLanguageId { get; set; }
+        public string Name { get; set; }
+    }
+
+
+
+    // 포켓몬 세팅 
+    public class PokemonCSV // (pokemon.csv)
+    {
+        //id,identifier,species_id,height,weight,base_experience,order,is_default
+
+        public int id { get; set; }                 // 데이터 ID. 폼 체인지는 10000부터 시작 
+        public string identifier { get; set; }      // 영문 명칭
+
+        public int SpeciesId { get; set; }
+
+        public int order { get; set;}               // 모든 폼 포함 순서. 검색할 때 보여줄 순서임
+
+        public bool isDefault { get; set; }         // 기본 폼인지 여부
+        
+
+    }
+
+    public class PokemonSpeciesNameCSV // (pokemon_species_names.csv)
+    {
+        public int SpeciesId { get; set; }                 // 포켓몬 종족 번호
+        public int LocalLanguageId { get; set; }    // 언어 ID. 1:일본어 3:한국어 9:영어
+        public string Name { get; set; }            // 언어에 따른 포켓몬 이름
+
+    }
+
+    public class PokemonMoveCSV // (pokemon_moves.csv)
+    {
+        public int PokemonId { get; set; }
+        public int VersionGroupId { get; set; }
+        public int MoveId { get; set; }
+        public int PokemonMoveMethodId { get; set; }
+        public int Level { get; set; }
+        public int Order { get; set; }
+    }
+
+}
---
+++ Services/CSVLoader.cs
@@ -0,0 +1,100 @@
+﻿using SMPage.Models;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SMPage.Services
+{
+
+
+    public static class CsvLoader
+    {
+        public static async Task<List<VersionCSV>> LoadVersions(HttpClient http, string path)
+        {
+            var csvData = await http.GetStringAsync(path);
+            return csvData.Split('\n')
+                         .Skip(1)
+                         .Where(line => !string.IsNullOrEmpty(line))
+                         .Select(line => line.Split(','))
+                         .Select(tokens => new VersionCSV
+                         {
+                             Id = int.Parse(tokens[0]),
+                             VersionGroupId = int.Parse(tokens[1]),
+                             Identifier = tokens[2]
+                         })
+                         .ToList();
+        }
+
+        public static async Task<List<VersionNameCSV>> LoadVersionNames(HttpClient http, string path)
+        {
+            var csvData = await http.GetStringAsync(path);
+            return csvData.Split('\n')
+                         .Skip(1)
+                         .Where(line => !string.IsNullOrEmpty(line))
+                         .Select(line => line.Split(','))
+                         .Select(tokens => new VersionNameCSV
+                         {
+                             VersionId = int.Parse(tokens[0]),
+                             LocalLanguageId = int.Parse(tokens[1]),
+                             Name = tokens[2]
+                         })
+                         .ToList();
+        }
+
+        public static async Task<List<PokemonSpeciesNameCSV>> LoadPokemonNames(HttpClient http, string path)
+        {
+            var csvData = await http.GetStringAsync(path);
+            return csvData.Split('\n')
+                       .Skip(1) // 헤더 줄 건너뛰기
+                       .Where(line => !string.IsNullOrEmpty(line))
+
+                       .Select(line => line.Split(','))
+                       .Select(tokens => new PokemonSpeciesNameCSV
+                       {
+                           SpeciesId = int.Parse(tokens[0]),
+                           LocalLanguageId = int.Parse(tokens[1]),
+                           Name = tokens[2]
+                       })
+                       .ToList();
+        }
+
+        public static List<PokemonCSV> LoadPokemons(string path)
+        {
+            return File.ReadAllLines(path)
+                       .Skip(1) // 헤더 줄 건너뛰기
+                       .Select(line => line.Split(','))
+                       .Select(tokens => new PokemonCSV
+                       {
+                           id = int.Parse(tokens[0]),
+                           identifier = tokens[1],
+                           SpeciesId = int.Parse(tokens[2]),
+                           order = int.Parse(tokens[3]),
+                           isDefault = bool.Parse(tokens[4])
+                       })
+                       .ToList();
+        }
+
+        public static List<PokemonMoveCSV> LoadPokemonMoves(string path)
+        {
+            return File.ReadAllLines(path)
+                       .Skip(1) // 헤더 줄 건너뛰기
+                       .Select(line => line.Split(','))
+                       .Select(tokens => new PokemonMoveCSV
+                       {
+                           PokemonId = int.Parse(tokens[0]),
+                           VersionGroupId = int.Parse(tokens[1]),
+                           MoveId = int.Parse(tokens[2]),
+                           PokemonMoveMethodId = int.Parse(tokens[3]),
+                           Level = int.Parse(tokens[4]),
+                           Order = int.TryParse(tokens[5], out var orderValue) ? orderValue : 0
+                       })
+                       .ToList();
+        }
+
+    }
+
+}
