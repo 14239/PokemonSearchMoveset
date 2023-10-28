82c297f8c01ef7e376a86c0985c293fd76163949
14239 <14239@naver.com>
2023-10-29 01:17:59 +0900

    Mudblazor 적용
    순서 이리저리 바꾸고 캐시 새로고침 하니까 해결완료됨

--- Program.cs
+++ Program.cs
@@ -7,8 +7,8 @@ using MudBlazor.Services;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddScoped<DataService>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<DataService>();
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
