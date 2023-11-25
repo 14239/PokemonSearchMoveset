7b77d77f0ad9f0d72db7ec8f6234334a19832e75
14239 <60643437+14239@users.noreply.github.com>
2023-11-25 08:54:16 +0000

    Deploying to gh-pages from @ 14239/PokemonSearchMoveset@aeafb1f34ee9924db1333c1edc154575bfcba4a7 🚀

---
+++ _content/BlazorPanzoom/blazorpanzoom.js
@@ -0,0 +1,97 @@
+﻿class BlazorPanzoomInterop {
+
+    constructor() {
+    }
+
+    fire(dispatcher, eventName, args) {
+        const name = "On" + eventName + "Event"
+        dispatcher.invokeMethodAsync(name, args)
+    }
+
+    createPanzoomForReference(element, options) {
+        const panzoom = Panzoom(element, options)
+        panzoom.boundElement = element
+        return panzoom
+    }
+
+    createPanzoomForSelector(selector, options) {
+        try {
+            const elements = document.querySelectorAll(selector)
+            const array = []
+            for (let i = 0; i < elements.length; i++) {
+                const element = elements[i]
+                array.push(DotNet.createJSObjectReference(this.createPanzoomForReference(element, options)))
+            }
+            return array
+        } catch {
+            throw new Error(`Cannot create a Panzoom object from selectors!`);
+        }
+    }
+
+    performForAllPanzoom(functionName, panzoomList, args) {
+        if (!panzoomList) {
+            return
+        }
+
+        for (let i = 0; i < panzoomList.length; i++) {
+            const ref = panzoomList[i]
+            ref[functionName](args)
+        }
+    }
+
+    registerSetTransform(dotnetReference, panzoom) {
+        if (!panzoom) {
+            return
+        }
+        const opts = {
+            setTransform: (elem, {x, y, scale, isSVG}) => {
+                this.fire(dotnetReference, 'SetTransform', {x, y, scale, isSVG})
+            }
+        }
+        panzoom.setOptions(opts)
+    }
+
+    registerZoomWithWheel(panzoom, element) {
+        const parent = element ? element.parentElement : panzoom.boundElement.parentElement
+        parent.addEventListener('wheel', panzoom.zoomWithWheel)
+    }
+
+    registerWheelListener(dotnetReference, panzoom, element) {
+        const parent = element ? element.parentElement : panzoom.boundElement.parentElement
+        parent.addEventListener('wheel', panzoom.boundWheelListener = this.wheelHandler.bind(this, dotnetReference))
+    }
+
+
+    wheelHandler(dotnetReference, event) {
+        event.preventDefault()
+        this.fire(dotnetReference, 'CustomWheel', {
+            deltaX: event.deltaX,
+            deltaY: event.deltaY,
+            clientX: event.clientX,
+            clientY: event.clientY,
+            shiftKey: event.shiftKey,
+            altKey: event.altKey
+        })
+    }
+
+    removeZoomWithWheel(panzoom, element) {
+        const parent = element ? element.parentElement : panzoom.boundElement.parentElement
+        parent.removeEventListener('wheel', panzoom.zoomWithWheel);
+    }
+
+    removeWheelListener(panzoom, element) {
+        const parent = element ? element.parentElement : panzoom.boundElement.parentElement
+        if (panzoom.boundWheelListener) {
+            parent.removeEventListener('wheel', panzoom.boundWheelListener);
+            delete panzoom.boundWheelListener
+        }
+    }
+
+    destroyPanzoom(panzoom) {
+        if (panzoom) {
+            delete panzoom.boundElement
+        }
+    }
+}
+
+window.blazorPanzoom = new BlazorPanzoomInterop()
---
+++ js/svgInteraction.js
@@ -0,0 +1,9 @@
+﻿// wwwroot/js/svgInteractions.js 또는 유사한 파일에 이 코드를 추가하세요.
+function addClickEventToSvgElement(svgElementId, dotNetReference, methodName) {
+    const element = document.getElementById(svgElementId);
+    if (element) {
+        element.addEventListener('click', event => {
+            dotNetReference.invokeMethodAsync(methodName, svgElementId);
+        });
+    }
+}
